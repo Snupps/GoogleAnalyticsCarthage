0d83cf88eb5b47a6b0ec7b9c64985088fd34093e
Peyman Khanjan <p.khanjan@gmail.com>
2016-10-21 15:28:11 +0100

    Google Analytics Carthage.

---
+++ GoogleAnalytics/GoogleAnalytics.h
@@ -0,0 +1,24 @@
+//
+//  GoogleAnalytics.h
+//  GoogleAnalytics
+//
+//  Created by Peyman Khanjan on 21/10/2016.
+//  Copyright © 2016 Snupps. All rights reserved.
+//
+
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+#import <GoogleAnalytics/GAIEcommerceFields.h>
+#import <GoogleAnalytics/GAIEcommerceProduct.h>
+#import <GoogleAnalytics/GAIEcommerceProductAction.h>
+#import <GoogleAnalytics/GAIEcommercePromotion.h>
+#import <GoogleAnalytics/GAIFields.h>
+#import <GoogleAnalytics/GAILogger.h>
+#import <GoogleAnalytics/GAITrackedViewController.h>
+#import <GoogleAnalytics/GAITracker.h>
+
+#import <GoogleAnalytics/TAGContainer.h>
+#import <GoogleAnalytics/TAGContainerOpener.h>
+#import <GoogleAnalytics/TAGDataLayer.h>
+#import <GoogleAnalytics/TAGLogger.h>
+#import <GoogleAnalytics/TAGManager.h>
---
+++ GoogleAnalytics/GoogleAnalytics.m
@@ -0,0 +1,12 @@
+//
+//  GoogleAnalytics.m
+//  GoogleAnalytics
+//
+//  Created by Peyman Khanjan on 21/10/2016.
+//  Copyright © 2016 Snupps. All rights reserved.
+//
+
+#import "GAI.h"
+
+@interface GAI (Dummy) @end
+@implementation GAI (Dummy) @end
